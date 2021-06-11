a3471499dab77f46604f9ff19f51cd9fc6277d94
Syriane-B <syriane.bonjean.simplon@gmail.com>
2021-06-11 14:11:32 +0200

    Level selectable

--- src/gameLogics/gameInitialisation.js
+++ src/gameLogics/gameInitialisation.js
@@ -3,8 +3,62 @@ import Ghost from "../models/Ghost";
 import { gameValues } from "./gameValues";
 import { screen, gameFloor, walls, foods } from "./mazeSetup";
 
-export function initGame() {
+export function startGame() {
+    // generate the level selector card card + title + select + button
+    const onBoardindCard = document.createElement('div');
+    onBoardindCard.style.width = "300px";
+    onBoardindCard.style.height = "300px";
+    onBoardindCard.style.border = "solid 3px #3F51B5";
+    onBoardindCard.style.display = "flex";
+    onBoardindCard.style.flexFlow = "column";
+    onBoardindCard.style.justifyContent = "center";
+    onBoardindCard.style.alignItems = "center";
+    onBoardindCard.style.backgroundColor = "black";
+    onBoardindCard.style.zIndex= 999;
+    onBoardindCard.style.position = "absolute";
+    onBoardindCard.style.left = `${gameValues.general.widthFloor/2 - 150}px`;
+    onBoardindCard.style.top = `${gameValues.general.heightFloor/2 - 150}px`;
+    const title = document.createElement('h3');
+    title.innerText = "Please choose a level";
+    title.style.color = "white";
+    onBoardindCard.appendChild(title);
+    const select = document.createElement('select');
+    select.id = "selectLevel";
+    onBoardindCard.appendChild(select);
+    const level1 = document.createElement('option');
+    level1.value = "1";
+    level1.innerText = "easy";
+    select.appendChild(level1);
+    const level2 = document.createElement('option');
+    level2.value = "2";
+    level2.innerText = "medium";
+    select.appendChild(level2);
+    const level3 = document.createElement('option');
+    level3.value = "3";
+    level3.innerText = "hard";
+    select.appendChild(level3);
+    const level4 = document.createElement('option');
+    level4.value = "4";
+    level4.innerText = "impossible";
+    select.appendChild(level4);
+    const startBtn = document.createElement('button');
+    startBtn.innerText = "Start !";
+    startBtn.addEventListener('click', () => {
+        // remove the card on boarding card
+        onBoardindCard.remove();
+        // init the game with the selected level
+        initGame(select.value);
+    });
+    onBoardindCard.appendChild(startBtn);
+
     const root = document.getElementById('root');
+    gameFloor.appendChild(onBoardindCard);
+    screen.appendChild(gameFloor);
+    root.appendChild(screen);
+}
+
+
+export function initGame(levelSelected) {
     let intervalGhostId = null;
     let ghosts = [];
 
@@ -36,7 +90,7 @@ export function initGame() {
             );
             ghosts.push(newGhost);
             gameFloor.appendChild(newGhost.getGhost());
-        }, 10000);
+        }, 10000 / (levelSelected * 0.6));
     }
     function GameOver() {
         const card = document.createElement('div');
@@ -95,8 +149,6 @@ export function initGame() {
     walls.forEach(wall => gameFloor.appendChild(wall));
     foods.forEach(food => gameFloor.appendChild(food));
     gameFloor.appendChild(pacMan.getPacMan());
-    screen.appendChild(gameFloor);
-    root.appendChild(screen);
 
 // We create the ghosts
     generateGhosts();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { initGame } from "./gameLogics/gameInitialisation";
+import { startGame } from "./gameLogics/gameInitialisation";
 import './style.scss';
 
-initGame();
+startGame();
